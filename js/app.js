73286e2cc4d35a785c1a4d7a4b90fce1189e6938
Darrin Howell <dbhowell09@gmail.com>
2018-09-11 17:29:05 -0700

    Corrected name variable

--- js/app.js
+++ js/app.js
@@ -12,30 +12,30 @@ console.log(visitorsName + 'is the user\'s name');
 //Question & Answer Variable Declarations
 
 // Question 1, plus feedback for correct and incorrect answers
-var favNBAPlayerQ = name + ', does Darrin think that Steph Curry is the best player in the NBA?';
+var favNBAPlayerQ = visitorsName + ', does Darrin think that Steph Curry is the best player in the NBA?';
 var favNBAPlayerAlertIfCorrect = 'Got \'em! Yup, no one else can make it splash like Steph.';
 var favNBAPLayerAlertIfWrong = 'Ah, sorry. It looks like you don\'t know Darrin as well as you thought.';
 
 // Question 2, plus feedback for correct and incorrect answers
-var favFoodTypeQ = name + ', does Darrin enjoy Filipino food more than any other?';
+var favFoodTypeQ = visitorsName + ', does Darrin enjoy Filipino food more than any other?';
 var favFoodTypeIfCorrect = 'You were right; although he has Filipino ancestry, his favorite'
                             + ' type of food is Japanese food.';
 var favFoodTypeIfWrong = 'Ah, bummer. Although Darrin is part Filipino, Filipino food'
                             + ' is not his number one. Japanese takes the top spot.';
 
 // Question 3, plus feedback for correct and incorrect answers
-var favCityQ = name + ', is Barcelona Darrin\'s favorite city?';
+var favCityQ = visitorsName + ', is Barcelona Darrin\'s favorite city?';
 var favCityIfCorrect = 'You were right! Barcelona is not his favorite city. PNW all the way';
 var favCityIfWrong = 'Sorry! Barcelona is a great city, but Seattle has his heart.';
 
 // Question 4, plus feedback for correct and incorrect answers
-var favHipHopQ = name + ', is Kendrick Darrin\'s favorite hip-hop artist?';
+var favHipHopQ = visitorsName + ', is Kendrick Darrin\'s favorite hip-hop artist?';
 var favHipHopIfCorrect = 'Yep, you were right: Darrin likes Lecrae most, not Kendrick';
 var favHipHopIfFalse = 'Ah, sorry, it\'s not Kendrick. It\'s Lecrae. Hipster, I know.';
 
 
 // Question 5, plus feedback for correct and incorrect answers
-var favAuthorQ = name + ', is Pierce Brown Darrin\'s favorite author?';
+var favAuthorQ = visitorsName + ', is Pierce Brown Darrin\'s favorite author?';
 var favAuthorIfCorrect = 'Yep, you were right! The Red Rising book series,'
                         + ' authored by Pierce Brown takes the cake for Darrin.';
 var favAuthorIfWrong = 'Sorry, unfortunately that\'s the wrong answer. Pierce Brown is the'
